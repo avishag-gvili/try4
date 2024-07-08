0f655edc9eae63374167108cee876d657ef00031
avishag-gvili <b3206003@gmail.com>
2024-07-08 13:22:25 +0300

    1/1

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = 3000;
 // });
 
 // app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example=== app listening at http://localhost:${port}`);
 // });
   // "dependencies": {
   //   "express": "^4.19.2"
